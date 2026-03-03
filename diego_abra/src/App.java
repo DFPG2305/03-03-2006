be6e06b7e55f6fdb0edde5c46d1a0159823ceb0b
Itseric2r <santiago.romero.restrepo@correounivalle.edu.co>
2026-03-03 15:47:10 -0500

    DiegoAbra

--- diego_abra/src/App.java
+++ diego_abra/src/App.java
@@ -3,7 +3,7 @@ public class App {
         System.out.println("Diego abra");
         int a = 0;
         for (int i = 0; i < 1000; i++) {
-            a += i;
+                a = a + 1;
             System.out.println("Diego abra " + a);
         }
     }
